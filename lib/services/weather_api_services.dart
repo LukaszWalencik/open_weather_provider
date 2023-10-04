15b0856dab2f7b60357e3e42dfb9c116272e9524
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-04 22:26:12 +0200

    Create WeatherApiServices to get directGeocoding

---
+++ lib/services/weather_api_services.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:open_weather_provider/constants/constants.dart';
+import 'package:open_weather_provider/exceptions/weatcher_exception.dart';
+import 'package:open_weather_provider/model/direct_geocoding.dart';
+import 'package:open_weather_provider/services/http_error_handler.dart';
+
+class WeatherApiServices {
+  final http.Client httpClient;
+  WeatherApiServices({
+    required this.httpClient,
+  });
+
+  Future<DirectGeocoding> getDirectGeocoding(String city) async {
+    final Uri uri = Uri(
+        scheme: 'https',
+        host: kApiHost,
+        path: '/geo/1.0/direct',
+        queryParameters: {
+          'q': city,
+          'limit': kLimit,
+          'appid': dotenv.env['APPID']
+        });
+    try {
+      final http.Response response = await httpClient.get(uri);
+      if (response.statusCode != 200) {
+        throw Exception(httpErrorHandler(response));
+      }
+      final responseBody = json.decode(response.body);
+
+      if (responseBody.isEmpty) {
+        throw WeatherException('Cannot get the locatiuon of $city');
+      }
+      final directGeocoding = DirectGeocoding.fromJson(responseBody);
+      return directGeocoding;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}

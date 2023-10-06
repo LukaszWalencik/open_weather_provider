35e60b5bf530a98eb49f49f68fbc8d47d9d6959e
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-06 21:13:02 +0200

    Add MultiProvider and inside Provider<WeatherRepository> and ChangeNotifierProvider<WeatherProvider>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:open_weather_provider/pages/home_page.dart';
+import 'package:open_weather_provider/providers/providers.dart';
+import 'package:open_weather_provider/repository/weather_repository.dart';
+import 'package:open_weather_provider/services/weather_api_services.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -13,14 +18,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weather App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        Provider<WeatherRepository>(
+          create: (context) => WeatherRepository(
+              weatherApiServices: WeatherApiServices(
+            httpClient: http.Client(),
+          )),
+        ),
+        ChangeNotifierProvider<WeatherProvider>(
+            create: (context) => WeatherProvider(
+                weatherRepository: context.read<WeatherRepository>()))
+      ],
+      child: MaterialApp(
+        title: 'Weather App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }

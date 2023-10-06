18d224b3e15d962250889afc8003916c6ffce9a0
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-06 21:05:14 +0200

    Create weather_provider

--- lib/providers/weather/weather_provider.dart
+++ lib/providers/weather/weather_provider.dart
@@ -1,45 +1,33 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
-
+import 'package:flutter/material.dart';
 import 'package:open_weather_provider/model/custom_error.dart';
 import 'package:open_weather_provider/model/weather.dart';
+import 'package:open_weather_provider/repository/weather_repository.dart';
 
 part 'weather_state.dart';
 
-enum WeatherStatus { initial, loading, success, error }
+class WeatherProvider with ChangeNotifier {
+  WeatherState _state = WeatherState.initial();
 
-class WeatherState extends Equatable {
-  final WeatherStatus status;
-  final Weather weather;
-  final CustomError error;
-  WeatherState({
-    required this.status,
-    required this.weather,
-    required this.error,
-  });
+  WeatherProvider({required this.weatherRepository});
+  WeatherState get state => _state;
 
-  factory WeatherState.initial() {
-    return WeatherState(
-        status: WeatherStatus.initial,
-        weather: Weather.initial(),
-        error: CustomError());
-  }
+  final WeatherRepository weatherRepository;
 
-  @override
-  List<Object> get props => [status, weather, error];
+  Future<void> fetchWeather(String city) async {
+    _state = _state.copyWith(status: WeatherStatus.loading);
+    print('State: $_state');
+    notifyListeners();
 
-  @override
-  bool get stringify => true;
-
-  WeatherState copyWith({
-    WeatherStatus? status,
-    Weather? weather,
-    CustomError? error,
-  }) {
-    return WeatherState(
-      status: status ?? this.status,
-      weather: weather ?? this.weather,
-      error: error ?? this.error,
-    );
+    try {
+      final Weather weather = await weatherRepository.fetchWeather(city);
+      _state = _state.copyWith(status: WeatherStatus.success, weather: weather);
+      print('State: $_state');
+    } catch (e) {
+      _state = _state.copyWith(
+          status: WeatherStatus.error,
+          error: CustomError(errorMsg: e.toString()));
+      print('State: $_state');
+    }
   }
 }
